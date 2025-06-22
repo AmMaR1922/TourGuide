b26a7c1bcc1d610c015081774ba05b5b58f3a08a
Mohamed Essam <Mohamed Essam@DESKTOP-SJSM9SF>
2025-06-22 15:38:14 +0300

    UnitOfWork And Repository

--- DomainLayer/Entities/Booking.cs
+++ DomainLayer/Entities/Booking.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DomainLayer.Common;
+using DomainLayer.Enums;
 
 namespace DomainLayer.Entities
 {
--- DomainLayer/Entities/Trip.cs
+++ DomainLayer/Entities/Trip.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DomainLayer.Common;
 
 namespace DomainLayer.Entities
 {
-   public class Trip : BaseEntity
+    public class Trip : BaseEntity
     {
         public string Name { get; set; } = null!;
         public string Duration { get; set; } = null!;
