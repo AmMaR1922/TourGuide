dadc30f1ad8b0ec62c78c44fc99072e4f13fcca5
Ammar <Ammar191230@gmail.com>
2025-06-26 02:39:14 +0300

    Add Repo Add Dto

---
+++ ApplicationLayer/Contracts/Repositories/CategoryRepoository/ICategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationLayer.Contracts.Repositories.CategoryRepoository
+{
+   public interface ICategoryRepository
+    {
+      // public Task<APIResponse<AddCategoryDto>> AddCategory(AddCategoryDto categoryDto);
+        //Task<APIResponse<List<Category>>> GetAllCategories();
+        //Task<APIResponse<Category>> GetCategoryById(int id);
+        //Task<APIResponse<bool>> DeleteCategory(int id);
+        //Task<APIResponse<Category>> UpdateCategory(int id, AddCategoryDto categoryDto);
+    }
+}
---
+++ TourGuide/Services/CategoryService/CategoryService.cs
@@ -0,0 +1,25 @@
+﻿using ApplicationLayer.Contracts.Repositories.CategoryRepoository;
+using ApplicationLayer.Contracts.UnitToWork;
+using ApplicationLayer.DTOs.CategoryDto;
+using ApplicationLayer.Models;
+using DomainLayer.Entities;
+
+namespace TourGuide.Services.CategoryService
+{
+    public class CategoryService : ICategoryRepository
+    {
+        public IUnitOfWork UnitOfWork { get; }
+
+        public CategoryService(IUnitOfWork unitOfWork)
+        {
+            UnitOfWork = unitOfWork;
+        }
+
+        //    public async Task<APIResponse<AddCategoryDto>> AddCategory(AddCategoryDto categoryDto)
+        //    {
+
+
+        //    }
+        //}
+    }
+}
