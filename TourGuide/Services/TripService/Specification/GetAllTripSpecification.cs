bc6ff7030dfe05ae1d60442a0f9b3221b2ce3975
Ammar <Ammar191230@gmail.com>
2025-06-26 06:30:46 +0300

    Add  TripService

---
+++ TourGuide/Services/TripService/Specification/GetAllTripSpecification.cs
@@ -0,0 +1,32 @@
+﻿using ApplicationLayer.Contracts.Specifications;
+using ApplicationLayer.Models.SpecificationParameters;
+using DomainLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace TourGuide.Services.TripService.Specification
+{
+    public class GetAllTripSpecification : BaseSpecification<Trip>
+    {
+        public GetAllTripSpecification(TripSpecParams Params)
+        {
+            Criteria = C =>
+            (
+                (string.IsNullOrEmpty(Params.Search) || C.Name.ToLower().Contains(Params.Search.ToLower()))
+            );
+
+            if (Params.sort == "asec")
+                OrderByAsec = C => C.CreatedAt;
+            else
+                OrderByDesc = C => C.CreatedAt;
+
+            IsPaginated = true;
+
+            if (IsPaginated)
+            {
+                ApplyPagination((Params.PageNumber - 1) * Params.PageSize, Params.PageSize);
+            }
+
+            Includes.Add(C => C.Include(C => C.Category));
+        }
+    }
+}
