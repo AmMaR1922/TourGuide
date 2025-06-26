cf1e250157bff8f3189ec45f5e159fcd94b67ec7
Ammar <Ammar191230@gmail.com>
2025-06-26 06:26:24 +0300

    Add ICategoryRepository

--- ApplicationLayer/Contracts/Repositories/CategoryRepoository/ICategoryRepository.cs
+++ ApplicationLayer/Contracts/Repositories/CategoryRepoository/ICategoryRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ApplicationLayer.DTOs.CategoryDto;
+using ApplicationLayer.Models;
+using ApplicationLayer.Models.SpecificationParameters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +11,13 @@ namespace ApplicationLayer.Contracts.Repositories.CategoryRepoository
 {
    public interface ICategoryRepository
     {
-      // public Task<APIResponse<AddCategoryDto>> AddCategory(AddCategoryDto categoryDto);
-        //Task<APIResponse<List<Category>>> GetAllCategories();
-        //Task<APIResponse<Category>> GetCategoryById(int id);
-        //Task<APIResponse<bool>> DeleteCategory(int id);
-        //Task<APIResponse<Category>> UpdateCategory(int id, AddCategoryDto categoryDto);
+      public Task<APIResponse<AddCategoryDto>> AddCategory(AddCategoryDto categoryDto);
+        public Task<APIResponse<List<CategoryDtoResponse>>> GetAllCategories(CategorySpecParams Params);
+
+        public Task<APIResponse<CategoryDtoResponse>> GetCategoryById(int Id);
+        public Task<APIResponse<UpdateCategoryDto>> UpdateCategory( AddCategoryDto categoryDto, int Id);
+        public Task<APIResponse<CategoryDtoResponse>> DeleteCategory(int Id);
+
+
     }
 }
