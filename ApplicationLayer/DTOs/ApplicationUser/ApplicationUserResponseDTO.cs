01298b34c9f43f726421952526a3a628412d1d9d
victor nisem <105614421+VICONISEM@users.noreply.github.com>
2025-06-27 02:27:40 +0300

    DTOs for Application user

---
+++ ApplicationLayer/DTOs/ApplicationUser/ApplicationUserResponseDTO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ApplicationLayer.DTOs.ApplicationUser
+{
+    public class ApplicationUserResponseDTO
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; } = null!;
+
+        public string UserName { get; set; } = null!;
+
+        public string Role { get; set; } = null!;
+
+        public string AccessToken { get; set; } = null!;
+
+        public DateTime RefreshTokenExperationDate { get; set; }
+        [JsonIgnore]
+        public string RefreshToken { get; set; } = null!;
+
+
+    }
+}
