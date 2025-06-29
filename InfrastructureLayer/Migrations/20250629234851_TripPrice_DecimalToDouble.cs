894c37ce6f805dd4ef295dd4dcec50340186402a
Mohamed Essam <Mohamed Essam@DESKTOP-SJSM9SF>
2025-06-30 02:49:41 +0300

    add-migration

---
+++ InfrastructureLayer/Migrations/20250629234851_TripPrice_DecimalToDouble.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InfrastructureLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class TripPrice_DecimalToDouble : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Price",
+                table: "Trips",
+                type: "float(10)",
+                precision: 10,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,2)",
+                oldPrecision: 10,
+                oldScale: 2);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Trips",
+                type: "decimal(10,2)",
+                precision: 10,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float(10)",
+                oldPrecision: 10,
+                oldScale: 2);
+        }
+    }
+}
