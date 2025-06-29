503c033b00ab3aa42c2c298b97ce09d5027cea9a
Mohamed Essam <Mohamed Essam@DESKTOP-SJSM9SF>
2025-06-29 21:18:23 +0300

    AddTripServices

---
+++ ApplicationLayer/Helper/FileHandler.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationLayer.Helper
+{
+    public static class FileHandler
+    {
+        public static async Task<string?> SaveFileAsync(string FolderName, IFormFile File)
+        {
+
+            if (File is null || File.Length == 0)
+            {
+                return null;
+            }
+            var FileName = FileNameConstructor(File.FileName);
+            var FilePath = Path.Combine(Directory.GetCurrentDirectory(), "ProtectedFiles", FolderName, FileName);
+
+            var directoryPath = Path.GetDirectoryName(FilePath);
+            if (!Directory.Exists(directoryPath))
+                Directory.CreateDirectory(directoryPath);
+
+            using (var FS = new FileStream(FilePath, FileMode.Create))
+            {
+                await File.CopyToAsync(FS);
+            }
+
+            return $"{FolderName}/{FileName}";
+        }
+
+        public static bool DeleteFile(string FilePath)
+        {
+            if (FilePath.IsNullOrEmpty())
+            {
+                return false;
+            }
+            var FullFilePath = Path.Combine(Directory.GetCurrentDirectory(), "ProtectedFiles", FilePath);
+
+            if (File.Exists(FullFilePath))
+            {
+                try
+                {
+                    string TempFilePath = Path.Combine(Directory.GetCurrentDirectory(), $"{Guid.NewGuid()}_{Path.GetFileName(FilePath)}");
+                    File.Move(FullFilePath, TempFilePath);
+                    File.Delete(TempFilePath);
+                    return true;
+                }
+                catch (IOException ex)
+                {
+                    Console.WriteLine($"Error deleting File: {ex.Message}");
+                    return false;
+                }
+            }
+
+            else { return false; }
+
+        }
+
+        private static string FileNameConstructor(string FileName)
+            => $"{DateTime.UtcNow.ToString("ddMMyy")}-{Guid.NewGuid()}-{FileName}";
+
+    }
+}
--- ApplicationLayer/Services/TripServices.cs
+++ ApplicationLayer/Services/TripServices.cs
@@ -53,14 +53,110 @@ namespace ApplicationLayer.Services
             return APIResponse<Pagination<TripDTOResponse>>.SuccessResponse(200, Pagination, "Trips retrieved successfully.");
         }
 
-        public Task<APIResponse<TripToBeReturnedDTO>> GetById(int Id)
+        public async Task<APIResponse<TripToBeReturnedDTO>> GetById(int Id)
         {
-            throw new NotImplementedException();
+            var trip = await unitOfWork.Repository<Trip>().GetAll().Where(t => t.Id == Id)
+                .Select(T => new TripToBeReturnedDTO()
+                {
+
+                    Name = T.Name,
+                    Duration = T.Duration,
+                    Description = T.Description,
+                    Price = T.Price,
+                    IsAvailable = T.IsAvailable,
+                    DateTime = T.DateTime,
+                    Rating = T.Rating,
+                    IsBestSeller = T.IsBestSeller,
+                    Category = T.Category.Name,
+                    MeetingPoint = T.MeetingPoint,
+                    Activities = T.Activities.Select(ta => ta.Activity.Name).ToList(),
+                    Languages = T.TripLanguages.Select(tl => tl.Language.Name).ToList(),
+                    Includes = T.TripIncludes.Where(ti => ti.IsIncluded).Select(ti => ti.Includes.Name).ToList(),
+                    NotIncludes = T.TripIncludes.Where(ti => !ti.IsIncluded).Select(ti => ti.Includes.Name).ToList(),
+                    MainImage = URLResolver.BuildFileUrl(T.TripImages.Where(i => i.IsMainImage).Select(img => img.ImageURL).FirstOrDefault()),
+                    Images = T.TripImages.Select(img => URLResolver.BuildFileUrl(img.ImageURL)).ToList(),
+                }).FirstOrDefaultAsync();
+
+            return trip is not null 
+                ? APIResponse<TripToBeReturnedDTO>.SuccessResponse(200, trip, "Trip retrieved successfully.")
+                : APIResponse<TripToBeReturnedDTO>.FailureResponse(404, null, "Trip not found.");
         }
 
-        public Task<APIResponse<string>> Add(TripToBeAddedDTO TripDto)
+        public async Task<APIResponse<string>> Add(TripToBeAddedDTO TripDto)
         {
-            throw new NotImplementedException();
+            var TripExists = unitOfWork.Repository<Trip>().GetAll().Any(t => t.Name == TripDto.Name);
+
+            if (TripExists)
+                return APIResponse<string>.FailureResponse(400, null, "Trip with the same name already exists.");
+
+            var Trip = new Trip()
+            {
+                Name = TripDto.Name,
+                Duration = TripDto.Duration,
+                Description = TripDto.Description,
+                Price = TripDto.Price,
+                IsAvailable = TripDto.IsAvailable,
+                DateTime = TripDto.DateTime,
+                CategoryId = TripDto.CategoryId,
+                MeetingPoint = new MeetingPoint
+                {
+                    Address = TripDto.MeetingPointAddress,
+                    URL = TripDto.MeetingPointURL
+                }
+            };
+
+            foreach (var activityId in TripDto.Activities)
+            {
+                var activity = await unitOfWork.Repository<Activity>().GetByIdAsync(activityId);
+                if (activity != null)
+                    Trip.Activities.Add(new TripActivities { Activity = activity });
+            }
+
+            foreach (var languageId in TripDto.Languages)
+            {
+                var language = await unitOfWork.Repository<Language>().GetByIdAsync(languageId);
+                if (language != null)
+                    Trip.TripLanguages.Add(new TripLanguages { Language = language });
+            }
+
+            foreach (var includeId in TripDto.Includes)
+            {
+                var include = await unitOfWork.Repository<Includes>().GetByIdAsync(includeId);
+                if (include != null)
+                    Trip.TripIncludes.Add(new TripIncludes { Includes = include, IsIncluded = true });
+            }
+
+            foreach (var notIncludeId in TripDto.NotIncludes)
+            {
+                var notInclude = await unitOfWork.Repository<Includes>().GetByIdAsync(notIncludeId);
+                if (notInclude != null)
+                    Trip.TripIncludes.Add(new TripIncludes { Includes = notInclude, IsIncluded = false });
+            }
+
+            foreach (var image in TripDto.Images)
+            {
+                if (image != null)
+                {
+                    var imageUrl = await FileHandler.SaveFileAsync("TripImages", image);
+                    if (imageUrl is not null)
+                        Trip.TripImages.Add(new TripImages { ImageURL = imageUrl, IsMainImage = false });
+                }
+            }
+
+            Trip.TripImages.Add(new TripImages()
+            {
+                ImageURL = await FileHandler.SaveFileAsync("TripImages", TripDto.MainImage),
+                IsMainImage = true
+            });
+
+
+            await unitOfWork.Repository<Trip>().AddAsync(Trip);
+            bool result = await unitOfWork.CompleteAsync();
+            if (!result)
+                return APIResponse<string>.FailureResponse(500, null, "Failed to add trip.");
+            
+            return APIResponse<string>.SuccessResponse(201, null, "Trip added successfully.");
+
         }
 
         public Task<APIResponse<string>> Delete(int Id)
