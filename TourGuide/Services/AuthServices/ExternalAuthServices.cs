4900b2e07a58af4b6fae960e3ddf7dd76d6e47a0
victor nisem <105614421+VICONISEM@users.noreply.github.com>
2025-07-01 02:12:26 +0300

    EditExternalAuthServices (Not Compeleted Yet )

--- TourGuide/Services/AuthServices/ExternalAuthServices.cs
+++ TourGuide/Services/AuthServices/ExternalAuthServices.cs
@@ -6,13 +6,15 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.SignalR;
 using System.Security.Claims;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace TourGuide.Services.AuthServices
 {
-    public class ExternalAuthServices(UserManager<ApplicationUser> userManager,ITokenServices token) : IExternalAuthService
+    public class ExternalAuthServices(UserManager<ApplicationUser> userManager,ITokenServices token , RoleManager<IdentityRole<int>> roleManager) : IExternalAuthService
     {
         private readonly ITokenServices tokenServices = token;
         private readonly UserManager<ApplicationUser> UserManager = userManager;
+        private readonly RoleManager<IdentityRole<int>> RoleManager = roleManager;
 
 
         public async Task<APIResponse<ApplicationUserResponseDTO>> HandelCallBack(AuthenticateResult result)
@@ -24,6 +26,9 @@ namespace TourGuide.Services.AuthServices
                 return APIResponse<ApplicationUserResponseDTO>.FailureResponse(400, new List<string> { "Error While Signin With Google" }, "Failed To Login or SignUp");
             }
             var IfLoginExited = await UserManager.FindByLoginAsync("Google", Sub);
+
+           
+
             if(IfLoginExited is not null)
             {
                 var ApplicationUserResponse = new ApplicationUserResponseDTO()
@@ -62,12 +67,171 @@ namespace TourGuide.Services.AuthServices
             }
             else
             {
-                throw new NotImplementedException();
+                var user = await UserManager.FindByEmailAsync(Email);
+
+                if(user is null)
+                {
+                    var UserName = Email.Substring(0,Email.IndexOf('@'));
+                    var NewUser = new ApplicationUser()
+                    {
+                        Email = Email,
+                        UserName = UserName,
+                    };
+
+
+                    var RT = tokenServices.GetRefreshToken();
+                    NewUser.RefreshTokens.Add(RT);
+                    var resultOfAdd =  await UserManager.CreateAsync(NewUser);
+                    if (!resultOfAdd.Succeeded)
+                    {
+                        var error = resultOfAdd.Errors.Select(e => e.Description).ToList();
+                        return APIResponse<ApplicationUserResponseDTO>.FailureResponse(400, error, "Error While Adding User");
+                    }
+
+                    if (!await RoleManager.RoleExistsAsync("NormalUser"))
+                    {
+                        return APIResponse<ApplicationUserResponseDTO>.FailureResponse(400, new List<string> { "Role not Exited" }, "Failed To Add User");
+
+                    }
+
+                    var AddRoleRes = await UserManager.AddToRoleAsync(NewUser,"NormalUser");
+                    if (!AddRoleRes.Succeeded)
+                    {
+                        var error = AddRoleRes.Errors.Select(e => e.Description).ToList();
+                        return APIResponse<ApplicationUserResponseDTO>.FailureResponse(400, error, "Error While Adding User To role");
+                    }
+
+                    var ApplicationUserResponse = new ApplicationUserResponseDTO() { 
+                    
+                         Id=NewUser.Id,
+                         Email=NewUser.Email,
+                         UserName = NewUser.UserName,
+                         Role = (await UserManager.GetRolesAsync(NewUser)).FirstOrDefault()??"Role",
+                         AccessToken = await tokenServices.GetAccessToken(NewUser),
+                         RefreshToken = RT.Token,
+                         RefreshTokenExperationDate =RT.ExpireOn
+
+                    
+                    };
+
+                    var provider = result.Ticket?.AuthenticationScheme;
+                   
+
+                    if(provider is null)
+                    {
+                        return APIResponse<ApplicationUserResponseDTO>.FailureResponse(400,null,"Error While Adding User To role");
+
+                    }
+
+                    var info = new UserLoginInfo(provider, Sub, provider);
+                    var linkResult = await UserManager.AddLoginAsync(NewUser, info);
+                    if (!linkResult.Succeeded)
+                    {
+                        var error = linkResult.Errors.Select(e => e.Description).ToList();
+                        return APIResponse<ApplicationUserResponseDTO>.FailureResponse(400, error, "Error While Adding User To role");
+
+                    }
+                   
+
+
+                    return APIResponse<ApplicationUserResponseDTO>.SuccessResponse(200, ApplicationUserResponse, "User Singup Succesfully");
+
+
+
+                }
+
+                else
+                {
+                    var ApplicationUserResponse = new ApplicationUserResponseDTO()
+                    {
+                        Id = user.Id,
+                        AccessToken = await tokenServices.GetAccessToken(user),
+                        Email = user.Email,
+                        UserName = user.UserName,
+                        Role  = (await UserManager.GetRolesAsync(user)).FirstOrDefault()??"Role",
+                        
+                    };
+
+                    if(user.RefreshTokens.Any(rt=>rt.IsActive))
+                    {
+                        var RT = user.RefreshTokens.FirstOrDefault(rt => rt.IsActive);
+                        ApplicationUserResponse.RefreshToken = RT.Token;
+                        ApplicationUserResponse.RefreshTokenExperationDate =RT.ExpireOn;
+
+                    }
+                    else
+                    {
+                        var RT = tokenServices.GetRefreshToken();
+                        user.RefreshTokens.Add(RT);
+                        var resultup = await UserManager.UpdateAsync(user);
+                        if(!resultup.Succeeded)
+                        {
+                            var errors = resultup.Errors.Select(e=>e.Description).ToList();
+                            return APIResponse<ApplicationUserResponseDTO>.FailureResponse(400, errors, "Error While Signin or Signup");
+                        }
+
+                        ApplicationUserResponse.RefreshToken = RT.Token;
+                        ApplicationUserResponse.RefreshTokenExperationDate=RT.ExpireOn;
+                      
+
+                    }
+
+                    var provider = result.Ticket?.AuthenticationScheme;
+
+
+                    if (provider is null)
+                    {
+                        return APIResponse<ApplicationUserResponseDTO>.FailureResponse(400, null, "Error While Adding User To role");
+
+                    }
+
+                    var info = new UserLoginInfo(provider, Sub, "GOOGLE");
+                    var linkResult = await UserManager.AddLoginAsync(user, info);
+                    if (!linkResult.Succeeded)
+                    {
+                        var error = linkResult.Errors.Select(e => e.Description).ToList();
+                        return APIResponse<ApplicationUserResponseDTO>.FailureResponse(400, error, "Error While Adding User To role");
+
+                    }
+                    return APIResponse<ApplicationUserResponseDTO>.SuccessResponse(200, ApplicationUserResponse, "User Singup Succesfully");
+
+
+
+                }
+
+               
             }
 
 
 
 
+        }
+
+        private async Task<APIResponse<ApplicationUserResponseDTO>> HandelRefreshToken(ApplicationUser User,ApplicationUserResponseDTO ApplicationUserResponse)
+        {
+            if (User.RefreshTokens.Any(Rt => Rt.IsActive))
+            {
+                var RT = User.RefreshTokens.FirstOrDefault(rt => rt.IsActive);
+                ApplicationUserResponse.RefreshTokenExperationDate = RT!.ExpireOn;
+                ApplicationUserResponse.RefreshToken = RT.Token;
+
+            }
+            else
+            {
+                var RT = tokenServices.GetRefreshToken();
+                User.RefreshTokens.Add(RT);
+                var res = await UserManager.UpdateAsync(User);
+                if (!res.Succeeded)
+                {
+                    var errors = res.Errors.Select(s => s.Description).ToList();
+                    return APIResponse<ApplicationUserResponseDTO>.FailureResponse(400, errors, "Failed To Login or SignUp");
+
+                }
+
+                ApplicationUserResponse.RefreshTokenExperationDate = RT!.ExpireOn;
+                ApplicationUserResponse.RefreshToken = RT.Token;
+            }
+            return APIResponse<ApplicationUserResponseDTO>.SuccessResponse(200, ApplicationUserResponse, "User Login Succesfully");
         }
     }
 }
