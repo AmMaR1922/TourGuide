d8490ceb62d22c614877e3981e2b86923a473bd3
victor nisem <105614421+VICONISEM@users.noreply.github.com>
2025-07-02 02:47:05 +0300

    Test Email Service

--- TourGuide/Controllers/ValuesController.cs
+++ TourGuide/Controllers/ValuesController.cs
@@ -1,19 +1,23 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using ApplicationLayer.Contracts.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace TourGuide.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
+ 
     
-    public class ValuesController : ControllerBase
+    public class ValuesController(IMailingService mailing) : ControllerBase
     {
+        private readonly IMailingService mailingService = mailing;
+
         [HttpGet]
-       
-        public IActionResult Get()
+        public async Task<IActionResult> Get()
         {
+            await mailingService.SendEmailAsync("victornisem01@gmail.com","Test Mail","Vico Gad3");
             return Ok("Dummy Data");
         }
     }
