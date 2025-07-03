61f26c8b3cf81026d1252b7d607d231aa3efd4fd
victor nisem <105614421+VICONISEM@users.noreply.github.com>
2025-07-04 02:30:32 +0300

    Add ActivityTranslationTable

---
+++ DomainLayer/Entities/ActivityTranslation.cs
@@ -0,0 +1,23 @@
+﻿using DomainLayer.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Entities
+{
+    public class ActivityTranslation:BaseEntity
+    {
+    
+        public int ActivityId { get; set; }
+        public Activity Activity { get; set; } = null!;
+        public int LanguageId { get; set; }
+        public Language Language { get; set; } = null!;
+        public string Name { get; set; } = null!;
+
+
+
+
+    }
+}
