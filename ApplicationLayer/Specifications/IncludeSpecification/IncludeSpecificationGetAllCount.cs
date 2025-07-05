b97d778d6ced12e8836afe5c6d35118bfc77e945
victor nisem <105614421+VICONISEM@users.noreply.github.com>
2025-07-05 03:16:00 +0300

    Make the Query Of Includes Have Serch And Result Is Pagenated

---
+++ ApplicationLayer/Specifications/IncludeSpecification/IncludeSpecificationGetAllCount.cs
@@ -0,0 +1,21 @@
+﻿using ApplicationLayer.Models;
+using ApplicationLayer.QueryParams;
+using DomainLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationLayer.Specifications.IncludeSpecification
+{
+    public class IncludeSpecificationGetAllCount:BaseSpecification<Includes>
+    {
+        public IncludeSpecificationGetAllCount(SpecParams spec) {
+
+            Criteria = i => (string.IsNullOrWhiteSpace(spec.Search) || EF.Functions.Like(i.Name, $"%{spec.Search}%"));
+
+        }
+    }
+}
